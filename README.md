221b6e0c0d165649b1d7232ec05f0ab729b0b233
Sil Kreulen <Sil.Kreulen-CIC.Netherlands@ibm.com>
2018-06-27 11:16:36 +0200

    first commit

--- README.md
+++ README.md
@@ -1,23 +1,14 @@
-# ReduxSimpleStarter
+# Simple Video Search Engine App build using React
+Build while following the course a course on [React-Redux](https://www.udemy.com/react-redux/)?
 
-Interested in learning [Redux](https://www.udemy.com/react-redux/)?
+### Aim
+Aim of this project was to get a basic understanding of React. Redux has not been applied in this application. Little effort has been put into styling.
 
-### Getting Started
+### Giphy
 
-There are two methods for getting started with this repo.
 
-#### Familiar with Git?
-Checkout this repo, install dependencies, then start the gulp process with the following:
-
-```
-> git clone https://github.com/StephenGrider/ReduxSimpleStarter.git
-> cd ReduxSimpleStarter
-> npm install
-> npm start
-```
-
-#### Not Familiar with Git?
-Click [here](https://github.com/StephenGrider/ReactStarter/releases) then download the .zip file.  Extract the contents of the zip file, then open your terminal, change to the project directory, and:
+#### Try it yourself ?
+Clone or download and:
 
 ```
 > npm install
--- src/index.js
+++ src/index.js
@@ -1,15 +1,47 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import YTSearch from 'youtube-api-search';
+import lodash from 'lodash';
+import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 
-import App from './components/app';
-import reducers from './reducers';
+const API_KEY = 'AIzaSyDB4WWMs9JfJsoD-O5vZQI0GfkA_DIEHOA';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+class App extends Component {
+  constructor(props) {
+    super(props);
 
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+    this.state = {
+      videos: [],
+      selectedVideo: null,
+    };
+
+    this.videoSearch('surfborads');
+  }
+
+  videoSearch(term) {
+    YTSearch({ key: API_KEY, term }, (videos) => {
+      this.setState({
+        videos, videos,
+        selectedVideo: videos[0],
+      });
+    });
+  }
+
+  render() {
+    const videoSearch = lodash.debounce((term) => { this.videoSearch(term) }, 300);
+
+    return (
+      <div>
+        <SearchBar onSearchTermChange={videoSearch}/>
+        <VideoList
+          onVideoSelect={selectedVideo => this.setState({selectedVideo})}
+          videos={this.state.videos}/>
+        <VideoDetail video={this.state.selectedVideo}/>
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<App />, document.querySelector('.container'));
